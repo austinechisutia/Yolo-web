f7dc1074b86cb41ba461ac05fec1e00ff621f205
Austine <austineakhonya624@gmail.com>
2025-09-18 21:43:03 +0300

    finalized the landing page

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
    const container = document.querySelector('.container-js');
     const elements = document.querySelectorAll('.star-js, .image-js');
 
-    container.addEventListener('mousemove', (e) => {
-      const { innerWidth, innerHeight } = window;
-      
-      // Calculate mouse position relative to center
-      const moveX = (e.clientX - innerWidth / 2) / 40; // smaller divisor = more movement
-      const moveY = (e.clientY - innerHeight / 2) / 40;
+    container.addEventListener("mousemove", (e)=>{
+        const { innerWidth, innerHeight } = window;
+        const moveX = (e.clientX-innerWidth/2)/10;
+        const moveY = (e.clientY - innerHeight/2)/10;
 
-      elements.forEach(el => {
-        el.style.transform = `translate(${moveX}px, ${moveY}px)`;
-      });
-    });
+        elements.forEach(el =>{
+            el.style.transform = `translate(${moveX}px, ${moveY}px)`;
+        })
+    })
